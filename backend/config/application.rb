4082449bdcbb7a4ebb7e900a9616fa71d0f54bd1
Sergio Willames Oliveira Costa <sergiowoc@gmail.com>
2023-02-08 02:39:00 -0300

    Feat/initial setup backend (#2)
    * gitignore file created
    
    * setting up email sending
    
    * installing and configuring the devise token auth
    
    * authentication configuration
    
    * endpoint for password change.
    
    * setting up the CORS Rack.
    
    * language change to portuguese.
    
    * configuring the tests with rspec.

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,6 +23,10 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # I18n config
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -34,6 +38,12 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.session_store :cookie_store,  key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+
     config.api_only = true
+
+    
   end
 end
